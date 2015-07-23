b4c7b51a118859302f14b4b095833b3d8d2493fb
taoyuan <torworx@gmail.com>
2015-07-23 18:07:18 +0800

    0.1.10

--- build/musher-all.js
+++ build/musher-all.js
@@ -1,11 +1,11 @@
 /***********************************************
-* Musher Javascript and Node.js Library v0.1.9
+* Musher Javascript and Node.js Library v0.1.10
 * https://github.com/taoyuan/musher
 * 
 * Copyright (c) 2015 Tao Yuan.
 * Licensed MIT 
 * 
-* Date: 2015-07-23 18:04
+* Date: 2015-07-23 18:07
 ***********************************************/
 // Only expose a single object name in the global namespace.
 // Everything must go through this module. Global Paho.MQTT module
